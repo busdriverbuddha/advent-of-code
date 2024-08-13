73512d31006baf62d6fee0f6f8f7b1e90add36b7
Guilherme Gama <gama@littlebrownmouse.com.br>
2024-08-13 14:03:14 -0300

    2015 day 1

---
+++ 2015/day01.py
@@ -0,0 +1,19 @@
+s = input()
+
+# Day 1
+resulting_floor = 0
+first_entered_basement = -1
+for i, c in enumerate(s, 1):
+    match c:
+        case '(':
+            resulting_floor += 1
+        case ')':
+            resulting_floor -= 1
+    if first_entered_basement == -1 and resulting_floor == -1:
+        first_entered_basement = i
+
+# Day 1
+print(resulting_floor)
+
+# Day 2
+print(first_entered_basement)
